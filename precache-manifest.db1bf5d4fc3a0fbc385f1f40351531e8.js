b67c48c196b5b70bdf3eb60021342d961e08cf80
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2020-02-19 11:51:56 +0100

    Updates

---
+++ precache-manifest.db1bf5d4fc3a0fbc385f1f40351531e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2301c20bca0708d2c5a553f656f5f986",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "271e6baf39c471a04f39",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9940d8d45269868b0b6b",
+    "url": "/robofriends/static/css/main.a5549f9e.chunk.css"
+  },
+  {
+    "revision": "271e6baf39c471a04f39",
+    "url": "/robofriends/static/js/2.754f7662.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.754f7662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9940d8d45269868b0b6b",
+    "url": "/robofriends/static/js/main.aa6a1f7d.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
